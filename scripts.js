9c2bf86b87304171148b91bac3872a576708f0b6
Robert-Gabriel UNGUREANU <robert.ungureanu@stud.acs.upb.ro>
2024-09-20 18:02:54 +0300

    New design, Color Picker, Eraser button, rainbow mode

--- scripts.js
+++ scripts.js
@@ -1,46 +1,130 @@
 const gridContainer = document.querySelector('.grid-container');
-const resetButton = document.querySelector('.reset');
-const normalSizeButton = document.querySelector('.normal');
 
-function calculateBoxSize(numRows,numCols) {
-    const gridContainerWidth = gridContainer.clientWidth;
-    const gridContainerHeight = gridContainer.clientHeight;
+const resetButton = document.querySelector('.clear');
+const drawButton = document.querySelector('.draw');
+const eraserButton = document.querySelector('.eraser');
+const selectButton = document.querySelector('.select');
+const rainbowButton = document.querySelector('.rainbow');
+const shadeButton = document.querySelector('.shade');
 
-    const boxWidth = (gridContainerWidth - numCols*2) / numCols;
-    const boxHeight = (gridContainerHeight - numRows*2) / numRows;
+const sliderValue = document.querySelector('.slider');
+const slider_span = document.querySelectorAll('.slider-span');
 
-    return {boxWidth, boxHeight};
-}
+const colorPicker = document.querySelector('#color-picker');
 
-function createGrid(numRows,numCols) {
-    for(let i = 0; i < numRows; i++) {
-        for(let j = 0; j < numCols; j++)
-        {
+const hiddenDiv = document.querySelector('.hidden');
+
+let randomColor = false;
+let shadeColor = false;
+
+let paintColor = 'black';
+
+function createGrid() {
+
+    const size = parseInt(sliderValue.value);
+   
+    gridContainer.innerHTML = '';
+    
+    let height = (gridContainer.clientHeight - 13) / size;
+    let width = (gridContainer.clientWidth - 7) / size;
+    
+    console.log(height*size, gridContainer.clientHeight);
+    console.log(width*size, gridContainer.clientWidth);
+
+    for(let i = 0; i < size*size; i++) {
+ 
             const gridItem = document.createElement('div');
             gridItem.classList.add('grid-item');
-            gridItem.style.backgroundColor = 'gray';
-
-            const {boxWidth, boxHeight} = calculateBoxSize(numRows,numCols);
-            gridItem.style.width = `${boxWidth}px`;
-            gridItem.style.height = `${boxHeight}px`;
-            
-
+            gridItem.style.width = height + 'px';
+            gridItem.style.height = width + 'px';
             gridContainer.appendChild(gridItem);
-        }
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    createGrid(30,30);
+    createGrid();
 });
 
 window.addEventListener('resize', () => {
     const gridItems = document.querySelectorAll('.grid-item');
-    const {boxWidth, boxHeight} = calculateBoxSize(30,30);
-
-    gridItems.forEach(gridItem => {
-        gridItem.style.width = `${boxWidth}px`;
-        gridItem.style.height = `${boxHeight}px`;
-    });
+    createGrid();
 });
+
+sliderValue.addEventListener('input', () => {
+    slider_span.forEach(span => {
+        span.textContent = sliderValue.value;
+    });
+    createGrid();
+});
+
+let menu = document.querySelector('.controls');
+
+
+menu.addEventListener('click', (event) => {
+    let target = event.target;
+    switch(target) {
+        case resetButton:
+            createGrid();
+            break;
+        case drawButton:
+            paintColor = colorPicker.value;
+            randomColor = false;
+            shadeColor = false;
+            break;
+        case eraserButton:
+            paintColor = 'white';
+            randomColor = false;
+            shadeColor = false;
+            break;
+        case selectButton:
+            hiddenDiv.classList.toggle('hidden');
+            break;
+        case rainbowButton:
+            randomColor = true;
+            shadeColor = false;
+            hiddenDiv.classList.toggle('hidden');
+            break;
+        case shadeButton:
+            hiddenDiv.classList.toggle('hidden');
+            shadeColor = true;
+            randomColor = false;
+            break;
+    }
+});
+
+gridContainer.addEventListener('mouseover', (event) => {
+    let target = event.target;
+    if(target.classList.contains('grid-item')) {
+        if(randomColor) {
+            paintColor = getRanonColor();
+        }
+        else if(shadeColor) {
+            paintColor = getShadeColor();
+        }
+        else if(paintColor === 'white') {
+            target.style.backgroundColor = paintColor;
+        }
+        target.style.backgroundColor = paintColor;
+    }
+});
+
+function getRanonColor() {
+    
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+}
+
+function getShadeColor() {
+    
+}
+
+
+
+
+
+
 
